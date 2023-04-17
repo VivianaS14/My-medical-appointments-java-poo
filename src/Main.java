e4eb22a79e239e0437883f15e9683e9ac3f54ab8
VivianaS14 <vasorianp@gmail.com>
2023-04-17 14:18:02 -0500

    Declare constants variables 'final'

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,6 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Alejandro Rodriguez");
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor myDoctorMin = new Doctor("Minnie");
-        myDoctorMin.showName();
-        myDoctorMin.showId();
-
         showMenu();
     }
 
