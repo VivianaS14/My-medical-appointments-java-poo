1a79c1036c395de6064350eb0f2d4f7bbe707382
VivianaS14 <vasorianp@gmail.com>
2023-04-17 15:45:51 -0500

    Getters and Setters in class Patient

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,13 @@ public class Main {
 
         Doctor myDoctor = new Doctor("Min Suga", "General");
 
+        Patient patient = new Patient("Victor", "victor@mail.com");
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("3057461180");
+        System.out.println(patient.getPhoneNumber());
+
     }
 
 
--- src/Patient.java
+++ src/Patient.java
@@ -1,5 +1,5 @@
 public class Patient {
-    int id;
+    private int id;
     private String name;
     private String email;
     private String address;
@@ -13,4 +13,84 @@ public class Patient {
         this.name = name;
         this.email = email;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() != 10) {
+            System.out.println("Número telefónico debe ser de 8 dígitos.");
+        } else {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getWeight() {
+        return this.weight + " Kg.";
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getHeight() {
+        return this.height + " Mts.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
+
+
+
 }
